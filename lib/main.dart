5fe70f38e68dae3322b290e02711e2f93553550a
Aji Anaz <aji.anaz@gmail.com>
2024-07-19 01:48:02 +0700

    Add : setup required package helper in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:magic_view/factory.dart';
+import 'package:magic_view/property/font/font.dart';
+import 'package:adk_tools/adk_tools.dart';
+import 'package:mellowminds/app/configs/constant.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ADKTools.init(
+    urlDev: urlDev,
+    apiKey: apiKey,
+    apiDevKey: apiKey,
+    appName: appName,
+    appFlavor: appFlavor,
+  );
+
+  MagicFactory.initMagicFactory(
+    colorBrand: Color(0xFF503E9D),
+    colorBrand2: Color(0xFF00cec9),
+    fontFamily: FontFamily.roboto,
+    fontName: fontName,
+  );
+
+  await ApiService().init();
+
   runApp(
     GetMaterialApp(
       title: "Mellow Minds",
