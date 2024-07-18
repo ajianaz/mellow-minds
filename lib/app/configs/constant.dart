a09b3bb3e12cfe2374ad78587457b0a4fc7bc2f1
Aji Anaz <aji.anaz@gmail.com>
2024-07-19 01:46:39 +0700

    Add : setup config folder

---
+++ lib/app/configs/constant.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:adk_tools/adk_tools.dart';
+
+const urlDev = "https://api.komikhub.lol";
+const apiKey = "komikhub";
+const appName = "Komikhub";
+const appFlavor = Flavor.development;
+
+const fontName = "Roboto";
